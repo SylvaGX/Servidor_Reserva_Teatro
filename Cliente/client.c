5bf0d4eb993497f529e420fe6377406f72bebfcc
TiagoMorais01 <58189630+TiagoMorais01@users.noreply.github.com>
2022-04-30 15:33:04 +0100

    Cliente V1.3 - Add Comentarios

--- Cliente/client.c
+++ Cliente/client.c
@@ -42,8 +42,8 @@ int main(int argc, char* argv[])
 
 	// create the socket  address (ip address and port)
 	server.sin_addr.s_addr = inet_addr("25.58.1.108");
-	server.sin_family = AF_INET;
-	server.sin_port = htons(80);
+	server.sin_family = AF_INET; 
+	server.sin_port = htons(80); //porta 80
 
 	//Connect to remote server
 	ws_result = connect(s, (struct sockaddr*)&server, sizeof(server));
@@ -54,28 +54,28 @@ int main(int argc, char* argv[])
 	}
 	int c;
 	printf("Insira a sua Localizacao(cidades e concelhos de Portugal):\n->");
-	scanf("%[a-zA-Z -]", local);
-	while ((c = getchar()) != '\n' && c != EOF);
+	scanf("%[a-zA-Z -]", local);//expressão regular para todos os caracteres de a a z, Espaço e - 
+	while ((c = getchar()) != '\n' && c != EOF); //clear "Enters"
 	system("cls");
 	
 	while (cont) {
 		printf("\n");
 		printf(" -----------\n");
-		printf("| 1) Teatros |\n| 2) Comprar |\n| 0) Sair    |\n");
+		printf("| 1) Teatros |\n| 2) Comprar |\n| 0) Sair    |\n"); //Menu
 		printf(" ------------\n->");
 		scanf("%c", &x);
 		while ((c = getchar()) != '\n' && c != EOF);
 		switch (x) {
 		case '0':
-			ZeroMemory(message, 1024);
+			ZeroMemory(message, 1024); //limpa o bloco da memória da variavel
 			strcpy(message, "400 BYE");
-			send(s, message, strlen(message), 0);
+			send(s, message, strlen(message), 0);//envio da messagem "400 BYE" para o servidor
 			cont = 0;
 			break;
 		case '1':
-			system("cls");
+			system("cls"); //limpa os caracteres do terminal
 			ZeroMemory(message, 1024);
-			strcpy(message, "TEATRO");
+			strcpy(message, "TEATRO");//Envio da messagem "TEATRO" para o servidor
 			ws_result = send(s, message, strlen(message), 0);
 
 			if (ws_result < 0)
@@ -86,23 +86,23 @@ int main(int argc, char* argv[])
 
 			ZeroMemory(message, 1024);
 			ZeroMemory(strRec, 1024);
-			bytesReceived = recv(s, strRec, 1024, 0);
+			bytesReceived = recv(s, strRec, 1024, 0); //receive 
 			if (bytesReceived == SOCKET_ERROR) {
 				printf("\nReceive error!\n");
 				cont = 0;
 				break;
 			}
 			if (bytesReceived > 0) {
-				if (strcmp(strRec, "100 OK") == 0) {
+				if (strcmp(strRec, "100 OK") == 0) { //caso de ter recebido a confirmacao 
 
 
 				}
-				else {
+				else { //caso de erro
 					cont = 0;
 				}
 			}
 
-			//esta variavel dps vai receber o valor 
+			//Envia a localizacao que a pessoa escreveu
 			ws_result = send(s, local, strlen(local), 0);
 			if (ws_result < 0)
 			{
@@ -111,45 +111,47 @@ int main(int argc, char* argv[])
 			}
 
 			ZeroMemory(strRec, 1024);
+			printf("A espera de acesso...\n ");
 			bytesReceived = recv(s, strRec, 1024, 0);
-			if (bytesReceived == SOCKET_ERROR) {
+			if (bytesReceived == SOCKET_ERROR) { //caso de erro
 				printf("\nReceive error!\n");
 				cont = 0;
 				break;
 			}
 			if (bytesReceived > 0) {
-				if (strcmp(strRec, "100 OK") == 0) {
-					//id 100ok nometeatro 100ok numespetaculos 100ok loc 100ok numvisitas 100ok
-					// 0         1                    2                3        4
+				if (strcmp(strRec, "100 OK") == 0) {//confirmacao
+					system("cls");
 					printf("\n");
 					ZeroMemory(strRec, 1024);
 					bytesReceived = recv(s, strRec, 1024, 0);
-					if (bytesReceived == SOCKET_ERROR) {
+					if (bytesReceived == SOCKET_ERROR) {//caso de erro
 						printf("\nReceive error!\n");
 						cont = 0;
 						err = 1;
 						break;
 					}
-					else if (bytesReceived == 0) {
+					else if (bytesReceived == 0) { //caso de erro 
 						err = 1;
 
 
 					}
 
 					while ((strcmp(strRec, "END") != 0)) {
+						//id 100ok nometeatro 100ok numespetaculos 100ok loc 100ok numvisitas 100ok
+						// 0         1                    2                3        4
 						if (contador < 4) {
-							printf("%s | ", strRec);
+							printf("%s | ", strRec);//imprime a informacao recebida
 							ZeroMemory(message, 1024);
 							strcpy(message, "100 OK");
 							send(s, message, strlen(message), 0);
-							contador++;
+							contador++; //incrementa no contador
 							ZeroMemory(strRec, 1024);
 							bytesReceived = recv(s, strRec, 1024, 0);
 
 						}
 						else if (contador == 4) {
-							contador = 0;
-							printf("%s\n", strRec);
+							contador = 0; //reset do contador
+							printf("%s\n", strRec);//imprime o numero de visitas e passa para a proximal linha
 							ZeroMemory(message, 1024);
 							strcpy(message, "100 OK");
 							send(s, message, strlen(message), 0);
@@ -205,6 +207,7 @@ int main(int argc, char* argv[])
 					}
 					ZeroMemory(message, 1024);
 					ZeroMemory(strRec, 1024);
+					printf("A espera de acesso...\n ");
 					bytesReceived = recv(s, strRec, 1024, 0);
 					if (bytesReceived == SOCKET_ERROR) {
 						printf("\nReceive error!\n");
@@ -213,6 +216,7 @@ int main(int argc, char* argv[])
 					}
 					if (bytesReceived > 0) {
 						if (strcmp(strRec, "100 OK") == 0) {
+							system("cls");
 							printf("\n");
 							ZeroMemory(strRec, 1024);
 							bytesReceived = recv(s, strRec, 1024, 0);
@@ -272,7 +276,7 @@ int main(int argc, char* argv[])
 								if (strcmp(strRec, "100 OK") == 0) {
 									printf("\n-1 para Sair");
 									printf("\nSelecione o numero do Teatro que ja visitou:\n->");
-									scanf("%d", &comprar);
+									scanf("%d", &comprar);//Escolha atraves do id
 									while ((c = getchar()) != '\n' && c != EOF);
 
 									char str[4];
@@ -295,15 +299,15 @@ int main(int argc, char* argv[])
 
 									if (bytesReceived > 0) {
 										printf("\n%s\n", strRec);
-										if (strcmp(strRec, "100 OK") == 0) {
+										if (strcmp(strRec, "100 OK") == 0) {//Id válido
 											system("cls");
 											printf("Compra efetuada com sucesso!\n\n");
 										}
-										if (strcmp(strRec, "300 LEFT") == 0) {
+										if (strcmp(strRec, "300 LEFT") == 0) {//Opcao -1 
 											system("cls");
 											printf("Pedido de compra cancelada!\n\n");
 										}
-										if (strcmp(strRec, "404 NOT FOUND") == 0) {
+										if (strcmp(strRec, "404 NOT FOUND") == 0) {//Id inválido
 											system("cls");
 											printf("Pedido de compra invalido. A opcao que inseriu nao existe!\n\n");
 										}
